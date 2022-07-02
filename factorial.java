59c9eddc06d706f280978ec248b02095091ad6d3
Madhav Narayan <madhavnarayan063@gmail.com>
2022-07-02 19:34:24 +0530

    Swited to new dev branch

--- factorial.java
+++ factorial.java
@@ -1,10 +1,15 @@
 /**
  * factorial
  */
+import java.util.*;
 public class factorial {
 
     public static void main(String[] args) {
-        
-        
+        Scanner sc = new Scanner(System.in);
+         int n= sc.nextInt();
+         System.out.println(fact(n));
+    }
+    static int fact(int n){
+        return n;
     }
 }
